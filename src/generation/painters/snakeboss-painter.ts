3b259208ceb99c8212fdfc04d0499df323dcba12
Theo Damaville | AT Internet <theo.damaville@atinternet.com>
2020-04-17 14:42:19 +0200

    bing bang

---
+++ src/generation/painters/snakeboss-painter.ts
@@ -0,0 +1,7 @@
+import { lineTile } from "../map_tiling";
+import { pointsOfRect } from "../map-geo";
+import { randomIn } from "../../game/utils/rectangle";
+
+export function snakeBoss(room, tilemapBg, tilemap) {
+    
+}

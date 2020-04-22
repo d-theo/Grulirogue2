57ab403a049117eab1c09dd9b154ee4e1757376f
Theo Damaville | AT Internet <theo.damaville@atinternet.com>
2020-04-22 09:04:53 +0200

    ci

--- husky.config.js
+++ husky.config.js
@@ -3,8 +3,8 @@ const tasks = arr => arr.join(' && ');
 module.exports = {
   'hooks': {
     'pre-push': tasks([
-      //'npm run bump-patch',
-      //`git tag ${require('./package.json').version}`
+      'npm version minor',
+      `git tag ${require('./package.json').version}`
     ])
   }
 }

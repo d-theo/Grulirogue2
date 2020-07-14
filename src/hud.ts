a92c941335ebf11847f892717e63ff8a0e29b616
d-theo <theo.damaville@gmail.com>
2020-07-14 10:56:45 +0200

    fix

--- src/hud.ts
+++ src/hud.ts
@@ -2,6 +2,12 @@ import { gameBus, playerMoved, gameStarted, logPublished, playerTookDammage, pla
 import $ from 'jquery';
 
 export function test() {
+    $('#log').keydown(function(e) {
+        // space and arrow keys
+        if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+        }
+    });
     gameBus.subscribe(gameStarted, event => {
         $('#hud').show();
         $('#log').show();
